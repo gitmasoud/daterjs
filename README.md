2acde94baae589d7781e88184a9a2928e574fb51
gitmasoud <shuaibam1@gmail.com>
2020-01-18 18:25:45 +0000

    Added new formats for date formatting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # daterjs
 daterRjs - a simple date formatter for your Node and JS projects.
+
+###### 
+###### Usage:
+###### 
+###### var { formatUK, formatUS } = require('daterjs');
+###### 
+###### console.log(formatUK('20,12,2020'));
+###### console.log(formatUS('20,12,2020'));
--- dateFrontEnd.js
+++ dateFrontEnd.js
@@ -1,13 +1,17 @@
 //my module
 const formats  = require('daterjs');
 
-//my date
-var dateToday = '2019:29:12';
-var dateToday2 = '2019:12:29';
+//my dates
+var date1 = '2019:29:12';
+var date2 = '2019:12:29';
+var date3 = '29:12:2019';
+var date4 = '12:29:2020';
+var ukDate = '12/29/2020';
 
 //usage
 console.log(formats.getTodaysDate());
-console.log(formats.formatUK(dateToday));
-console.log(formats.formatUS(dateToday));
-console.log(formats.formatDayDateYearTimeFormatter1(dateToday));
-console.log(formats.formatDayDateYearTimeFormatter2(dateToday2));
+console.log('\x1b[36m%s\x1b[0m',formats.getTodaysDateUK(ukDate));
+console.log(formats.formatUK(date1));
+console.log(formats.formatUS(date1));
+console.log(formats.formatDayDateYearTimeFormatter1(date1));
+console.log(formats.formatDayDateYearTimeFormatter2(date2));
